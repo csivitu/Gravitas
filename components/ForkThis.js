bcd0ae915cf703304fdb7a702c2b7de0c7a98b4b
Nimish Jain <nimish@Nimish.local>
2022-09-18 17:55:49 +0530

    fix: UI Fixes for events

--- components/ForkThis.js
+++ components/ForkThis.js
@@ -2,29 +2,31 @@ import React from 'react'
 import Button from './Button'
 
 import Lottie from "lottie-react";
-import globeAnimation from "../public/images/laserTag.json";
+import globeAnimation from "../public/images/forkThis.json";
 
 function ForkThis() {
     return (
         <div id="LaserTag" className="flex h-screen justify-center items-center">
             <div className="flex justify-around px-40">
                 <div className="flex flex-col">
-                    <div className='flex flex-col w-full items-start'>
-                        <p className='font-bold'>🤩 EVENT 1</p>
-                        <img src="images/lasertagLogo.svg" className='h-16'/>
+                    <div className='flex flex-col items-end justify-end'>
+                        <p className='font-bold mr-2'>🤩 EVENT 2</p>
+                        <p className='font-inter text-6xl'>Forkthis</p>
                     </div>
                     <div className="flex mt-16 items-center gap-20">
-                    <Lottie animationData={globeAnimation} style={{ height: 800 }} />
-                        <div className='flex flex-col items-start'>
-                            <p className='font-light text-lg mb-5'>
-                                A great disturbance in the force can be felt again. Is it the rise of a skywalker or the rise of a phantom menace?
-                                Come join CSI-VIT in a battle of lasertag lightsabers to help restore balance to the force.
+                        
+                        <div className='flex flex-col items-end w-1/2'>
+                            <p className='font-light text-lg mb-5 text-right'>
+                                CSI-VIT presents ForkThis, a workshop where you learn and contribute to the world of open-source with zero prerequisites. Resolve issues and eliminate errors to win exciting prizes. Collaborate with developers, learn new skills, and take Hacktoberfest by storm!
                             </p>
                             <p className='font-medium mb-12'>
                                 For there is only do or do not, there is no try.
                             </p>
-                            <br/>
-                            <Button text={"Coming Soon!"}/>
+                            <br />
+                            <Button text={"Coming Soon!"} />
+                        </div>
+                        <div className='w-1/2'>
+                            <Lottie animationData={globeAnimation} style={{height: "100%"}} />
                         </div>
                     </div>
                 </div>
--- components/LaserTag.js
+++ components/LaserTag.js
@@ -7,30 +7,27 @@ import Image from 'next/image';
 
 function LaserTag() {
     return (
-        <div id="LaserTag" className="flex h-screen justify-center items-center">
-            <div className="flex justify-around px-40">
-                <div className="flex flex-col">
-                    <div className='flex flex-col w-full items-start'>
-                        <p className='font-bold'>🤩 EVENT 1</p>
-                        <br/>
-                        <Image src="/images/lasertagLogo.svg" height="96px" width="420px" />
+        <div id="LaserTag" className="flex h-screen justify-center items-center px-40">
+            <div className="flex flex-col">
+                <div className='flex flex-col w-full items-start'>
+                    <p className='font-bold ml-2'>🤩 EVENT 1</p>
+                    <Image src="/images/lasertagLogo.svg" height="96px" width="420px" />
+                </div>
+                <div className="flex items-center gap-20 mb-10">
+                    <div className='w-1/2'>
+                        <Lottie animationData={globeAnimation} style={{height: "100%"}} />
                     </div>
-                    <div className="flex items-center gap-20 mb-10">
-                    <Lottie animationData={globeAnimation} style={{ height: 800 }} />
-                        <div className='flex flex-col items-start'>
-                            <p className='font-light text-lg mb-5'>
-                                A great disturbance in the force can be felt again. Is it the rise of a skywalker or the rise of a phantom menace?
-                                Come join CSI-VIT in a battle of lasertag lightsabers to help restore balance to the force.
-                            </p>
-                            <p className='font-medium mb-12'>
-                                For there is only do or do not, there is no try.
-                            </p>
-                            <br/>
-                            <Button text={"Coming Soon!"}/>
-                        </div>
+                    <div className='flex flex-col items-start w-1/2'>
+                        <p className='font-light text-lg mb-5'>
+                            A great disturbance in the force can be felt again. Is it the rise of a skywalker or the rise of a phantom menace?
+                            Come join CSI-VIT in a battle of lasertag lightsabers to help restore balance to the force.
+                        </p>
+                        <p className='font-medium mb-12'>
+                            For there is only do or do not, there is no try.
+                        </p>
+                        <Button text={"Coming Soon!"} />
                     </div>
                 </div>
-
             </div>
         </div>
     )
