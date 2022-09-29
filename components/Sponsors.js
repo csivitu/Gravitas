11b79fb735b9b05f2e62f7976b59bd07b385bcf8
Kush <goyal.kush@hotmail.com>
2022-09-29 14:55:33 +0530

    feat: spons final

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -1,9 +1,9 @@
-import { ParallaxBanner, ParallaxProvider } from "react-scroll-parallax"
+// import { ParallaxBanner, ParallaxProvider } from "react-scroll-parallax"
 
 const Sponsors = () => {
     return (
         <div id="Sponsors" className="z-40" layers={[{ image: '/assets/starbg.png', speed: 30 }]} >
-            <section className="w-full justify-evenly items-center text-center flex flex-col p-4 sm:p-10 " style={{ backgroundColor: "#16191B" }} >
+            <section className="w-full justify-evenly items-center text-center flex flex-col p-4 sm:p-10 " style={{ backgroundColor: "#000000" }} >
                 <div className="flex justify-center flex-col items-center">
                     <p className="font-semibold text-3xl pt-10">
                         MEET OUR <span className="text-ds-blue font-semibold">SPONSORS</span>
@@ -16,8 +16,8 @@ const Sponsors = () => {
                     <p className="text-ds-blue font-semibold text-xl  mb-2">
                         TECH SPONSOR
                     </p>
-                    <a href="https://www.experionglobal.com/" target="_blank" rel="noreferrer" className="relative flex justify-center items-center p-3 sm:p-5 z-50">
-                        <img src="/assets/sponsor_logos/experion.png" className="w-1/2 sm:w-full h-auto" />
+                    <a href="https://nextechbyamson.com" target="_blank" rel="noreferrer" className="relative flex justify-center items-center p-3 sm:p-5 z-50">
+                        <img src="/images/light logo.png" className="w-1/2 sm:w-full h-auto" />
                     </a>
                 </div>
                 <div className="flex flex-col w-screen justify-center items-center mt-10" id="diamond">
@@ -25,18 +25,18 @@ const Sponsors = () => {
                         MERCHADISE SPONSORS
                     </p>
                     <div className="w-full grid grid-cols-3 content-center  place-items-center gap-2 mt-2 p-3 sm:p-5">
-                        <a href="https://witnet.io/" target="_blank" rel="noreferrer" className="z-50 w-2/4">
-                            <img src='/assets/sponsor_logos/wilnet.png' className=" h-auto" />
+                        <a href="https://comfortlady.co.in/" target="_blank" rel="noreferrer" className="z-50 w-2/4">
+                            <img src='/images/ComfortLady.png' className=" h-auto" />
                         </a>
-                        <a href="https://oceanprotocol.com/" target="_blank" rel="noreferrer" className="z-50 w-2/4 h-auto" >
-                            <img src='/assets/sponsor_logos/op.png' />
+                        <a href="https://comfortlady.co.in/" target="_blank" rel="noreferrer" className="z-50 w-2/4 h-auto" >
+                            <img src='/images/Galxy.png' />
                         </a>
-                        <a href="https://synthetix.io/" target="_blank" rel="noreferrer" className="z-50 w-2/4 h-auto">
-                            <img src="/assets/sponsor_logos/SNX.svg" className="z-50" />
+                        <a href="https://comfortlady.co.in/" target="_blank" rel="noreferrer" className="z-50 w-2/4 h-auto">
+                            <img src="/images/ComfortMan.png" className="z-50" />
                         </a>
                     </div>
                 </div>
-                <div className="flex flex-col w-full justify-center items-center mt-10" id="diamond">
+                {/* <div className="flex flex-col w-full justify-center items-center mt-10" id="diamond">
                     <p className="text-ds-blue font-semibold text-xl mb-2">
                         DIAMOND PARTNERS
                     </p>
@@ -48,8 +48,8 @@ const Sponsors = () => {
                             <img src="/assets/sponsor_logos/motorq.png" className="mx-auto w-1/2 sm:w-3/4" />
                         </a>
                     </div>
-                </div>
-                <div className="my-10">
+                </div> */}
+                {/* <div className="my-10">
                     <p className="text-ds-blue font-semibold text-xl mb-2">
                         GOLD PARTNER
                     </p>
@@ -65,7 +65,7 @@ const Sponsors = () => {
                             </a>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </section>
         </div>
     )
